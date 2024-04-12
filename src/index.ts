34f5f282d940f826046cf65f946474ca5d97fcc8
joben <jobenapao10@gmail.com>
2024-04-12 16:53:43 +0800

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import {v2 as cloudinary} from 'cloudinary'
-import "dotenv/config";
-import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute";
-import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { v2 as cloudinary } from 'cloudinary';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import restaurantRoute from './routes/RestaurantRoute';
+import myUserRoute from './routes/MyUserRoute';
+import myRestaurantRoute from './routes/MyRestaurantRoute';
 
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("Connected to the database"));
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log('Connected to the database'));
 
-
-  //cloudinary config
+//cloudinary config
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -24,18 +22,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/health", async (req: Request, res: Response) => {
-  res.send({
-    message: "Health OK",
-  });
+app.get('/health', async (req: Request, res: Response) => {
+	res.send({
+		message: 'Health OK',
+	});
 });
 
 //Endpoints
-app.use("/api/my/user", myUserRoute);
-app.use("/api/my/restaurant", myRestaurantRoute)
+app.use('/api/restaurant', restaurantRoute);
+app.use('/api/my/user', myUserRoute);
+app.use('/api/my/restaurant', myRestaurantRoute);
 
 const PORT = 7000 || process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Server started at port ${PORT}`);
+	console.log(`Server started at port ${PORT}`);
 });
---
+++ src/routes/RestaurantRoute.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { param } from 'express-validator';
+import { searchRestaurant } from '../controllers/RestaurantController';
+
+const router = express.Router();
+
+// /api/restaurant/search/manila
+router.get(
+	'/search/:city',
+	param('city').isString().trim().notEmpty().withMessage('City must be a valid string'),
+	searchRestaurant
+);
+
+export default router;
